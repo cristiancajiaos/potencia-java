2087a9c2b98320880c6b314cbd719610f8b8fda5
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-05-31 14:16:06 -0400

    Añadida implementación

--- src/potencia/Potencia.java
+++ src/potencia/Potencia.java
@@ -1,20 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package potencia;
 
-/**
- *
- * @author crist
- */
+
 public class Potencia {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int resultado = 1;
+        int numeroBase = 2;
+        
+        int potencia = 22;
+        
+        for (int i = 1; i <= potencia; i++) {
+            resultado *= numeroBase;
+        }
+        
+        System.out.println("Resultado: " + resultado);
     }
     
 }
